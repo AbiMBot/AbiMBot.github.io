d286d4a5e068f0400a3134d0537596dbf7aeaa61
idbart <44906437+idbart@users.noreply.github.com>
2020-07-31 20:48:56 -0400

    changed header font and added style to the 404 page

--- assets/js/components/notFoundComponent.js
+++ assets/js/components/notFoundComponent.js
@@ -1,7 +1,9 @@
 const notFoundComponent = {
 	template: `
-		<div class="not-found-wrapper">
-			404 Page Not Found!
+		<div class="not-found-wrapper span">
+			<div class="not-found span span-screen-height flex-row flex-center">
+				<h1> <i class="fas fa-exclamation-circle"></i> 404 Page Not Found! </h1>
+			</div>
 			<footing></footing>
 		</div>
 	`,
