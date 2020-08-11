6814a07f1b29fb980ff2c638036d019378bc5cbf
idbart <44906437+idbart@users.noreply.github.com>
2020-08-10 20:41:50 -0400

    added text to the top of the rates page and edited the pricing card component to support bullet points

--- assets/js/components/global/pricingCardComponent.js
+++ assets/js/components/global/pricingCardComponent.js
@@ -2,12 +2,14 @@ Vue.component('pricing-card', {
 	template: `
 		<div class="pricing-card-wrapper flex-column flex-center bg-accent color-secondary animate__animated animate__fadeIn">
 			<h2>{{title}}</h2>
-			<div class="pricing-card-photos">{{photos}} photographs</div>
+			<ul class="pricing-card-bullets-wrapper">
+				<li v-for="bullet in bullets" class=""> {{bullet}} </li>
+			</ul>
 			<div class="pricing-card-price font-text"><strong>{{'$' + price}}</strong></div>
 		</div>
 	`,
 
-	props: ['title', 'price', 'photos'],
+	props: ['title', 'price', 'bullets'],
 	data: function() {
 		return {
 
@@ -15,5 +17,5 @@ Vue.component('pricing-card', {
 	},
 	methods: {
 
-	}
+	},
 });
--- assets/js/components/ratesComponent.js
+++ assets/js/components/ratesComponent.js
@@ -1,8 +1,9 @@
 const ratesComponent = {
 	template: `
 		<div class="rates-wrapper span flex-column flex-center">
+			<p class="pricing-intro color-accent">Contact me if you have questions or need more information regarding photography services. In special circumstances rates may be adjusted to better suit the client. I take pride in being flexible and being able to work with you in whatever location, time frame, and price range works best for both the photographer and client. </p>
 			<div class="pricing-packages flex-row flex-wrap flex-center">
-				<pricing-card v-for="package in packages" v-bind:title="package.name" v-bind:price="package.price" v-bind:photos="package.photos"><pricing-card>
+				<pricing-card v-for="package in packages" v-bind:title="package.name" v-bind:price="package.price" v-bind:bullets="package.bullets"><pricing-card>
 			</div>
 			<footing></footing>
 		<div>
@@ -15,37 +16,85 @@ const ratesComponent = {
 				{
 					name: "Engagement Session",
 					price: 110,
-					photos: 30
+					bullets: [
+						"30 photographs",
+						"One hour session",
+						"One location",
+						"One outfit change if desired",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
 				},
 				{
 					name: "Wedding",
 					price: 400,
-					photos: 150
+					bullets: [
+						"$100 security deposit",
+						"150 photographs",
+						"Meeting in advance",
+						"Coverage of the entire day beginning with getting ready",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
 				},
 				{
 					name: "Family Session",
 					price: 75,
-					photos: 15
+					bullets: [
+						"15 photographs",
+						"30 minute session",
+						"One location",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
 				},
 				{
 					name: "Senior Session",
 					price: 100,
-					photos: 20
+					bullets: [
+						"20 Photographs",
+						"One hour session",
+						"One location",
+						"One outfit change if desired",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
 				},
 				{
 					name: "Maternity Session",
 					price: 100,
-					photos: 20
+					bullets: [
+						"20 photographs",
+						"One hour session",
+						"One location",
+						"One outfit change if desired",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
 				},
 				{
 					name: "Model Session",
 					price: 75,
-					photos: 15
+					bullets: [
+						"15 photographs",
+						"30 minute session",
+						"One location",
+						"One outfit change if desired",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
 				},
 				{
 					name: "Newborn/Kids Session",
 					price: 75,
-					photos: 15
+					bullets: [
+						"10 photographs",
+						"45 minute session",
+						"One location",
+						"One outfit change if desired",
+						"Your choice of digital images on a USB, or an online gallery"
+					]
+				},
+				{
+					name: "Event Coverage",
+					price: 75,
+					bullets: [
+						"Pay by the hour"
+					]
 				}
 			]
 		};
